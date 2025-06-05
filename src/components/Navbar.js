9a17da7f2ac70ea132943de555602f8de5a246b0
piipeares <piipeares@gmail.com>
2025-06-05 19:44:23 -0300

    a

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,11 @@ const Navbar = () => {
   return (
     <header className="bg-gradient-to-r from-blue-700 to-blue-900 text-white shadow-md">
       <div className="container mx-auto flex justify-between items-center py-4 px-6">
-        {/* Logo */}
+        
         <div className="text-2xl font-bold cursor-pointer">
           MovieApp
         </div>
 
-        {/* Navegación */}
         <nav>
           <ul className="flex space-x-8 text-lg">
             <li className="hover:text-yellow-400 cursor-pointer transition-colors duration-300">
